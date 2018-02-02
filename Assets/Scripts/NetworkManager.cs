48b198bfded981b48afc6d6152d7740ca11e6418
marckvs <al341516@uji.es>
2018-02-02 12:04:07 +0100

    primer commit

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+    public string version;
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings(version);
+	}
+
+    void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Gloabal", new RoomOptions() { maxPlayers = 2 }, null);
+    }
+	
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Personaje", transform.position, transform.rotation, 0);
+    }
+	
+}
---
+++ Assets/Scripts/Sync.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sync : Photon.MonoBehaviour {
+
+    public Vector3 RealPosition = Vector3.zero;
+    public Quaternion RealRotation = Quaternion.identity;
+
+    public Vector3 RealPosition1 = Vector3.zero;
+    public Quaternion RealRotation1 = Quaternion.identity;
+    public Vector3 RealVelocity1 = Vector3.zero;
+
+    public Vector3 RealVelocity2 = Vector3.zero;
+
+    public Rigidbody rg;
+	
+	// Update is called once per frame
+
+    void FixedUpdate()
+    {
+        if (!photonView)
+        {
+            rg.position = Vector3.Lerp(rg.position, RealPosition1, 0.004f);
+            rg.rotation = Quaternion.Lerp(rg.rotation, RealRotation1, 0.004f);
+            rg.velocity = Vector3.Lerp(rg.velocity, RealVelocity1, 0.004f);
+            rg.angularVelocity = Vector3.Lerp(rg.angularVelocity, RealVelocity2, 0.004f);
+        }
+    }
+	void Update () {
+
+        if (!photonView){
+            transform.position = Vector3.Lerp(transform.position, RealPosition, 0.004f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, RealRotation, 0.004f);
+        }
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+
+            stream.SendNext(rg.position);
+            stream.SendNext(rg.rotation);
+            stream.SendNext(rg.velocity);
+            stream.SendNext(rg.angularVelocity);
+        }
+
+        else
+        {
+            RealPosition = (Vector3)stream.ReceiveNext();
+            RealRotation = (Quaternion)stream.ReceiveNext();
+
+            RealPosition1 = (Vector3)stream.ReceiveNext();
+            RealRotation1 = (Quaternion)stream.ReceiveNext();
+            RealVelocity1 = (Vector3)stream.ReceiveNext();
+            RealVelocity2 = (Vector3)stream.ReceiveNext();
+
+
+        }
+    }
+}
